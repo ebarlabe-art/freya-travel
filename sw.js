db6b54479b97ad74af480ff6122f64d78de63c79
Eva Barlabe <ebarlabe@gmail.com>
2026-08-30 18:27:41 +0200

    Remove legacy service worker conflict

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='freya-travel-v6.4.9-activity-deeplinks';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./itinerary.html','./freya-travel-v1.5/itinerary.html'];
+const CACHE='freya-travel-v6.4.10-legacy-sw-cleanup';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./itinerary.html','./freya-travel-v1.5/index.html','./freya-travel-v1.5/itinerary.html'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
@@ -12,8 +12,11 @@ self.addEventListener('fetch',event=>{
   const isIndexRequest=isSameOrigin && (url.pathname.endsWith('/index.html') || url.pathname === '/' || url.pathname === '/freya-travel/' || url.pathname === '/freya-travel');
   if(isNavigation || isIndexRequest){
     const rootItineraryPath=new URL('./itinerary.html',self.location.href).pathname;
+    const nestedIndexPath=new URL('./freya-travel-v1.5/index.html',self.location.href).pathname;
+    const nestedAppPath=new URL('./freya-travel-v1.5/',self.location.href).pathname;
     const nestedItineraryPath=new URL('./freya-travel-v1.5/itinerary.html',self.location.href).pathname;
-    const fallbackKey=url.pathname===rootItineraryPath?'./itinerary.html':url.pathname===nestedItineraryPath?'./freya-travel-v1.5/itinerary.html':'./index.html';
+    const isNestedIndex=url.pathname===nestedIndexPath||url.pathname===nestedAppPath||url.pathname===nestedAppPath.slice(0,-1);
+    const fallbackKey=url.pathname===rootItineraryPath?'./itinerary.html':url.pathname===nestedItineraryPath?'./freya-travel-v1.5/itinerary.html':isNestedIndex?'./freya-travel-v1.5/index.html':'./index.html';
     event.respondWith(fetch(request).then(response=>{
       if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(fallbackKey,copy))}
       return response;
